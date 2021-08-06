284bf1189cd18385665f3ff86b6ad750d146fbe4
Cathy <smile25896@gmail.com>
2021-08-06 19:33:43 +0800

    新增： 搜尋價格功能

---
+++ js/house-data.js
@@ -0,0 +1,159 @@
+let priceList = [];
+
+$(document).ready(function () {
+  setPriceCheckboxesEvent();
+  setPriceInputEvent();
+});
+
+function compare(a, b) {
+  if (a === "max") return 1;
+  if (b === "max") return -1;
+  return Number(a) - Number(b);
+}
+
+function setPriceCheckboxesEvent() {
+  let priceCheckboxes = getPriceCheckboxes();
+
+  priceCheckboxes.forEach((item) => {
+    item.addEventListener("click", handleClickChangePriceCheckbox);
+  });
+}
+
+function getPriceCheckboxes() {
+  let priceCheckboxes = document.querySelectorAll(
+    'input[name="search-select-option-price"'
+  );
+  return Array.apply(null, priceCheckboxes);
+}
+
+function handleClickChangePriceCheckbox(e) {
+  setPriceList(e.target);
+  let priceCheckboxes = getPriceCheckboxes();
+  if (e.target.value === "unlimit" && e.target.checked) {
+    priceCheckboxes.slice(1).forEach((item) => {
+      item.checked = false;
+    });
+  } else if (e.target.value === "unlimit" && !e.target.checked) {
+    e.target.checked = true;
+  } else if (priceCheckboxes[0].checked) {
+    priceCheckboxes[0].checked = false;
+  }
+}
+
+function setPriceList(target, needCheckMiddle = true) {
+  let { value, checked } = target;
+  let text = "總價不限";
+  if (value === "unlimit") {
+    priceList.length = 0;
+  } else if (checked) {
+    let price = value.split("-");
+    priceList.push(price[0]);
+    priceList.push(price[1]);
+    priceList = priceList.filter(function (item, pos, self) {
+      return self.indexOf(item) == pos;
+    });
+    priceList = priceList.sort(compare);
+
+    if (needCheckMiddle) {
+      checkedMiddlePrice(priceList[0], priceList[priceList.length - 1]);
+    }
+    text = setPriceText();
+  } else {
+    let count = 0;
+
+    let priceCheckboxes = getPriceCheckboxes();
+    for (let i = 1; i < priceCheckboxes.length; i++) {
+      if (priceCheckboxes[i].checked) {
+        count++;
+      }
+    }
+    let price = value.split("-");
+    if (count < 1) {
+      priceList.length = 0;
+    } else {
+      if (price[0] === priceList[0]) {
+        priceList.splice(0, 1);
+      } else if (price[1] === priceList[priceList.length - 1]) {
+        priceList.pop();
+      } else {
+        priceList = price;
+        priceCheckboxes.forEach((item) => {
+          item.checked = false;
+        });
+        target.checked = true;
+      }
+    }
+    text = setPriceText();
+  }
+  document.querySelector(".search-select-price").textContent = text;
+}
+
+function setPriceText() {
+  let text = "";
+  if (
+    priceList.length === 0 ||
+    (priceList[0] === "0" && priceList[priceList.length - 1] === "max")
+  ) {
+    text = "總價不限";
+  } else if (priceList[0] === "0" || priceList[0] === "") {
+    text = priceList[priceList.length - 1] + "萬以下";
+  } else if (
+    priceList[priceList.length - 1] === "max" ||
+    priceList[priceList.length - 1] === ""
+  ) {
+    text = priceList[0] + "萬以上";
+  } else {
+    text = priceList[0] + "萬-" + priceList[priceList.length - 1] + "萬";
+  }
+  return text;
+}
+
+function checkedMiddlePrice(minPrice, maxPrice) {
+  let priceCheckboxes = getPriceCheckboxes();
+  priceCheckboxes = priceCheckboxes.slice(2);
+  priceCheckboxes.pop();
+  priceCheckboxes.forEach((item) => {
+    let price = item.value.split("-");
+    if (
+      Number(price[0]) > minPrice &&
+      (maxPrice === "max" || Number(price[1]) < maxPrice)
+    ) {
+      item.checked = true;
+      setPriceList(item, false);
+    }
+  });
+}
+
+function setPriceInputEvent() {
+  let priceInput1 = document.querySelector('input[name="price1"]');
+  let priceInput2 = document.querySelector('input[name="price2"]');
+
+  priceInput1.addEventListener("input", handleInputPrice);
+  priceInput2.addEventListener("input", handleInputPrice);
+
+  priceInput1.addEventListener("change", handleChangePrice);
+  priceInput2.addEventListener("change", handleChangePrice);
+}
+
+function handleInputPrice(e) {
+  let priceCheckboxes = getPriceCheckboxes();
+  priceCheckboxes.forEach((item) => {
+    item.checked = false;
+  });
+}
+
+function handleChangePrice(e) {
+  let priceInput1 = document.querySelector('input[name="price1"]');
+  let priceInput2 = document.querySelector('input[name="price2"]');
+
+  priceList.length = 0;
+  priceList.push(priceInput1.value);
+  priceList.push(priceInput2.value);
+
+  if (priceList[0] !== "" && priceList[1] !== "") {
+    priceList.sort(compare);
+  }
+
+  let text = setPriceText();
+  document.querySelector(".search-select-price").textContent = text;
+}
--- js/house.js
+++ js/house.js
@@ -5,24 +5,14 @@ $(document).ready(function () {
     item.addEventListener("click", handleClickTab);
   });
 
-  // let selects = document.querySelectorAll(".search-select");
-  // selects = Array.apply(null, selects);
-  // selects.forEach((item) => {
-  //   item.addEventListener("click", handleClickDropdown);
-  // });
-
-  // document.addEventListener("click", blurDropdown);
-
   $(".dropdown-box a").on("click", function (event) {
     let isShow = event.target.parentNode.classList.contains("show1");
     $(".dropdown-box").removeClass("show1");
-    console.dir(event.target.parentNode);
     if (isShow) {
       event.target.parentNode.classList.remove("show1");
     } else {
       event.target.parentNode.classList.add("show1");
     }
-    // $(this).parent().toggleClass("show1");
   });
 });
 
@@ -36,21 +26,6 @@ $("body").on("click", function (e) {
   }
 });
 
-// function blurDropdown() {
-//   console.log("blur");
-//   let dropdowns = document.querySelectorAll(".dropdown");
-//   dropdowns = Array.apply(null, dropdowns);
-//   dropdowns.forEach((item) => {
-//     item.classList.remove("active");
-//   });
-
-//   let dropdownButtons = document.querySelectorAll(".dropdown-button");
-//   dropdownButtons = Array.apply(null, dropdownButtons);
-//   dropdownButtons.forEach((item) => {
-//     item.classList.remove("active");
-//   });
-// }
-
 function handleClickTab(e) {
   let tabList = document.querySelectorAll(".m-search-for-lists li");
   tabList = Array.apply(null, tabList);
@@ -60,22 +35,3 @@ function handleClickTab(e) {
 
   e.target.classList.add("is-active");
 }
-
-// function handleClickDropdown(e) {
-//   console.log("click");
-//   e.preventDefault();
-//   console.dir(e.target);
-//   let target = e.target;
-//   console.log("." + target.dataset.dropdown);
-//   if (target.classList.contains("active")) {
-//     document
-//       .querySelector("." + target.dataset.dropdown)
-//       .classList.remove("active");
-//     target.classList.remove("active");
-//   } else {
-//     document
-//       .querySelector("." + target.dataset.dropdown)
-//       .classList.add("active");
-//     target.classList.add("active");
-//   }
-// }
